11f280c3dc1716dba70e633ec916f70776d79a04
Suboda-NeuraIntelligence <suboda@neuraintelligence.com>
2024-03-14 00:06:16 +0530

    removing unnecessary components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import "./App.css";
-import AboutUs from "./components/home/AboutUs";
-import ContactUs from "./components/home/ContactUs";
-import Footer from "./components/home/Footer";
 import HeroSection from "./components/home/HeroSection";
-import OurProducts from "./components/home/OurProducts";
-import OurServices from "./components/home/OurServices";
-import OurTeam from "./components/home/OurTeam";
-import OurTechStack from "./components/home/OurTechStack";
+import "./App.css";
 
 function App() {
   return (
-    <div className="">
+    <div>
       <HeroSection />
-      <OurServices/>
-      <OurProducts />
-      <AboutUs/>
-      <OurTechStack/>
-      <OurTeam/>
-      <ContactUs/>
-      <Footer/>
     </div>
   );
 }
